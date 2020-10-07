60d3edfc805bae04285bbf24cdde6c0bbba7bd0c
denisgandza <gandja.denis@yandex.ru>
2020-10-07 23:02:10 +0300

    Insertion sorting

---
+++ src/java/InsertionSort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 4, 3, 0, 19, 13, 456, 33, 22, 55, 6, 8};
+        insertionSort(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int value = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > value) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = value;
+        }
+    }
+}
